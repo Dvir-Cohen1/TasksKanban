8e2fd3750ae13319c0787b158ffc452cc839a9ef
Dvir Cohen <dvir906@gmail.com>
2023-02-07 12:36:05 +0200

    start users page

--- client/src/routes/_routes.js
+++ client/src/routes/_routes.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-const Login = React.lazy(() => import("../pages/authentication/Login"));
 const Tasks = React.lazy(() => import("../pages/tasks/Tasks"));
 const Index = React.lazy(() => import("../pages/Index"));
+const Users = React.lazy(() => import("../pages/users/Users"));
+
+// Auth
+const Login = React.lazy(() => import("../pages/authentication/Login"));
 const Register = React.lazy(() => import("../pages/authentication/Register"));
 
 export const appRoutes = [
   { linkLabel: "Dashboard", path: "/", component: Index, isProtected: true },
+  { linkLabel: "Users", path: "/users", component: Users, isProtected: true },
   { linkLabel: "Tasks", path: "/tasks", component: Tasks, isProtected: true },
 
   // Auth Routes
   { linkLabel: "Login", path: "/login", component: Login, isAuthRoute: true },
-  { linkLabel: "Register", path: "/register", component: Register, isAuthRoute: true },
+  {
+    linkLabel: "Register",
+    path: "/register",
+    component: Register,
+    isAuthRoute: true,
+  },
 ];
